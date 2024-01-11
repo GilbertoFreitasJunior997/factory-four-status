9f4e7f422ecb14d2b72798bb0b66d8bdbf2d6fc8
Gilberto de Freitas <gilberto@frgnet.com.br>
2024-01-11 13:50:09 -0300

    feat: added status checks with react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,63 @@
+import axios from "axios";
+import { useQueries } from "@tanstack/react-query";
+
+type Status = {
+  success: boolean;
+  message: string;
+  hostname: string;
+  time: Date;
+};
+const ENDPOINTS = [
+  "accounts",
+  "assets",
+  "customers",
+  "datapoints",
+  "devices",
+  "documents",
+  "forms",
+  "invites",
+  "media",
+  "messages",
+  "namespaces",
+  "orders",
+  "patients",
+  "relationships",
+  "rules",
+  "templates",
+  "users",
+  "workflows",
+];
+
+const getSampleStatus = async (endpoint: string) =>
+  (
+    await axios.get<Status>(
+      `https://api.factoryfour.com/${endpoint}/health/status`
+    )
+  ).data;
+
+const queries = ENDPOINTS.map((endpoint) => ({
+  queryKey: [endpoint],
+  queryFn: () => getSampleStatus(endpoint),
+}));
+
 export const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const statuses = useQueries({
+    queries,
+  });
+
+  return (
+    <div className="w-screen h-screen grid grid-cols-5">
+      {statuses.map((status, index) => (
+        <div key={index} className="w-full h-40">
+          {status.isLoading
+            ? "LOADING"
+            : status.error
+            ? "QUERY ERROR"
+            : status.data?.success
+            ? "SUCCESS"
+            : "ERROR"}
+        </div>
+      ))}
+    </div>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import "./index.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { App } from "./app";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const client = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
