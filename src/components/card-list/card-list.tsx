0fac0395ae57eac0bd8ac99753a8371c72db496d
Beto Freitas <gilberto@frgnet.com.br>
2024-01-14 02:07:49 -0300

    fix(card-list): adjusted grid-cols for responsivity

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -3,7 +3,7 @@ import { SERVICES } from "../../api/services";
 
 export const CardList = () => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       {SERVICES.map((service) => (
         <Card service={service} key={service} />
       ))}
